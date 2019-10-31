e7e1d23fc12b37bc56071ef161156157062a4165
7evanator <57014883+7evanator@users.noreply.github.com>
2019-10-30 20:24:19 -0500

    new function
    dollarcounter

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ document.getElementById("app").innerHTML = `
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
+<div id="myDiv"></div>
 `;
 
+document.getElementById("myDiv").innerHTML = "jojo";
 console.log("this is amazing");
 console.log("00000");
 console.log("0   0");
 console.log("0   0");
 console.log("0   0");
 console.log("00000");
+
+function dollarCounter(singles, fives, tens, twnties) {
+  var total = singles + fives + tens + twnties;
+  console.log("you have " + total + " bills");
+  if (total > 150) {
+    console.log("your rich");
+  }
+}
+dollarCounter(50, 10, 5, 4);
